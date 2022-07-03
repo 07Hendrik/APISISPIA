83ce9cf6c872b28076ae20e39a1d8e613b9a909d
Henry <henrydanteg10@gmail.com>
2022-07-02 22:09:25 -0500

    Actualizado

--- src/routes/persona.js
+++ src/routes/persona.js
@@ -1,71 +1,70 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
-const BD = require('../config/configbd');
+const BD = require("../config/configbd");
 
 //READ
-router.get('/getUsers', async (req, res) => {
-    sql = "select * from tabla_persona where idpersona=1";
+router.get("/getUsers", async (req, res) => {
+  sql = "select * from tabla_persona where idpersona=1";
 
-    let result = await BD.Open(sql, [], false);
-    Users = [];
+  let result = await BD.Open(sql, [], false);
+  Users = [];
 
-    result.rows.map(user => {
-        let userSchema = {
-            "idpersona": user[0],
-            "dni": user[1],
-            "nombre": user[2],
-            "apepaterno": user[3]
-        }
+  result.rows.map((user) => {
+    let userSchema = {
+      idpersona: user[0],
+      dni: user[1],
+      nombre: user[2],
+      apepaterno: user[3],
+    };
 
-        Users.push(userSchema);
-    })
+    Users.push(userSchema);
+  });
 
-    res.json(Users);
-})
+  res.json(Users);
+});
 
 //CREATE
-/*
-router.post('/addUser', async (req, res) => {
-    const { username, firstname, lastname } = req.body;
+router.post("/addUser", async (req, res) => {
+  const { idpersona, dni, nombre, apepaterno, apematerno } = req.body;
 
-    sql = "insert into person(username,firstname,lastname) values (:username,:firstname,:lastname)";
+  console.log(idpersona);
+  sql =
+    "insert into tabla_persona(idpersona,dni,nombre, apepaterno, apematerno) values (:idpersona,:dni,:nombre, :apepaterno, :apematerno)";
 
-    await BD.Open(sql, [username, firstname, lastname], true);
+  await BD.Open(sql, [idpersona,dni,nombre, apepaterno, apematerno], true);
 
-    res.status(200).json({
-        "username": username,
-        "firstname": firstname,
-        "lastname": lastname
-    })
-})
+  res.status(200).json({
+    idpersona: idpersona,
+    dni: dni,
+    nombre: nombre,
+  });
+});
 
 //UPDATE
 router.put("/updateUser", async (req, res) => {
-    const { codu, username, firstname, lastname } = req.body;
+  const { idpersona, nombre, apepaterno, apematerno } = req.body;
 
-    sql = "update person set username=:username, firstname=:firstname, lastname=:lastname where codu=:codu";
+  sql =
+    "update tabla_persona set nombre=:nombre, apepaterno=:apepaterno, apematerno=:apematerno where idpersona=:idpersona";
 
-    await BD.Open(sql, [username, firstname, lastname,codu], true);
+  await BD.Open(sql, [nombre, apepaterno, apematerno, idpersona], true);
 
-    res.status(200).json({
-        "codu": codu,
-        "username": username,
-        "firstname": firstname,
-        "lastname": lastname
-    })
-
-})
+  res.status(200).json({
+    idpersona: idpersona,
+    nombre: nombre,
+    apematerno: apematerno
+  });
+});
 
 //DELETE
-router.delete("/deleteUser/:codu", async (req, res) => {
-    const { codu } = req.params;
+router.delete("/deleteUser", async (req, res) => {
+  const { idpersona } = req.params;
 
-    sql = "update person set state=0 where codu=:codu";
+  sql = "delete from tabla_persona where idpersona=:idpersona";
 
-    await BD.Open(sql, [codu], true);
+  await BD.Open(sql, [idpersona], true);
 
-    res.json({ "msg": "Usuario Eliminado" })
-})
-*/
+  res.json({ msg: "Usuario Eliminado" });
+});
 
 module.exports = router;
